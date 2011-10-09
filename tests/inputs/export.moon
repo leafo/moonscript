7f55485e62e7196c977988861641f486cf448370
leaf corcoran <leafot@gmail.com>
2011-10-09 02:00:25 -0700

    tests for special export symbols

---
+++ tests/inputs/export.moon
@@ -0,0 +1,37 @@
+
+What = if this
+  232
+else
+  4343
+
+export a,b,c
+
+a,b,c,d = "hello"
+
+export ^
+
+another = 3434
+Another = 7890
+
+if inner then Yeah = "10000"
+
+What = if this
+  232
+else
+  4343
+
+export *
+
+What = if this
+  232
+else
+  4343
+
+x,y,z = 1,2,3
+
+y = ->
+  hallo = 3434
+
+with tmp
+  j = 2000
+
---
+++ tests/outputs/export.lua
@@ -0,0 +1,31 @@
+local What
+if this then
+  What = 232
+else
+  What = 4343
+end
+local d
+a, b, c, d = "hello"
+local another = 3434
+Another = 7890
+if inner then
+  local Yeah = "10000"
+end
+if this then
+  What = 232
+else
+  What = 4343
+end
+if this then
+  What = 232
+else
+  What = 4343
+end
+x, y, z = 1, 2, 3
+y = function()
+  local hallo = 3434
+end
+do
+  local _with_0 = tmp
+  local j = 2000
+end
