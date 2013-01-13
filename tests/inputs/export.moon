d772547d59436fa56587e1250ef2a44932fc9fd2
leaf corcoran <leafot@gmail.com>
2013-01-13 11:50:26 -0800

    update export tests

--- tests/inputs/export.moon
+++ tests/inputs/export.moon
@@ -1,44 +1,50 @@
 
-export a,b,c = 223, 343
-export cool = "dad"
+do
+  export a,b,c = 223, 343
+  export cool = "dad"
 
-export class Something
-  umm: "cool"
+do
+  export class Something
+    umm: "cool"
 
-What = if this
-  232
-else
-  4343
+do
+  export a,b,c
+  a,b,c,d = "hello"
 
-export a,b,c
 
-a,b,c,d = "hello"
+do
+  What = if this
+    232
+  else
+    4343
 
-export ^
+  export ^
 
-another = 3434
-Another = 7890
+  another = 3434
+  Another = 7890
 
-if inner then Yeah = "10000"
+  if inner then Yeah = "10000"
 
-What = if this
-  232
-else
-  4343
+  What = if this
+    232
+  else
+    4343
 
-export *
 
-What = if this
-  232
-else
-  4343
+do
+  export *
 
-x,y,z = 1,2,3
+  What = if this
+    232
+  else
+    4343
 
-y = ->
-  hallo = 3434
+  x,y,z = 1,2,3
 
-with tmp
-  j = 2000
+  y = ->
+    hallo = 3434
+
+  with tmp
+    j = 2000
 
 
--- tests/outputs/export.lua
+++ tests/outputs/export.lua
@@ -1,73 +1,83 @@
-a, b, c = 223, 343
-cool = "dad"
 do
-  local _parent_0 = nil
-  local _base_0 = {
-    umm = "cool"
-  }
-  _base_0.__index = _base_0
-  if _parent_0 then
-    setmetatable(_base_0, _parent_0.__base)
-  end
-  local _class_0 = setmetatable({
-    __init = function(self, ...)
-      if _parent_0 then
-        return _parent_0.__init(self, ...)
-      end
-    end,
-    __base = _base_0,
-    __name = "Something",
-    __parent = _parent_0
-  }, {
-    __index = function(cls, name)
-      local val = rawget(_base_0, name)
-      if val == nil and _parent_0 then
-        return _parent_0[name]
-      else
-        return val
-      end
-    end,
-    __call = function(cls, ...)
-      local _self_0 = setmetatable({}, _base_0)
-      cls.__init(_self_0, ...)
-      return _self_0
+  a, b, c = 223, 343
+  cool = "dad"
+end
+do
+  do
+    local _parent_0 = nil
+    local _base_0 = {
+      umm = "cool"
+    }
+    _base_0.__index = _base_0
+    if _parent_0 then
+      setmetatable(_base_0, _parent_0.__base)
     end
-  })
-  _base_0.__class = _class_0
-  if _parent_0 and _parent_0.__inherited then
-    _parent_0.__inherited(_parent_0, _class_0)
+    local _class_0 = setmetatable({
+      __init = function(self, ...)
+        if _parent_0 then
+          return _parent_0.__init(self, ...)
+        end
+      end,
+      __base = _base_0,
+      __name = "Something",
+      __parent = _parent_0
+    }, {
+      __index = function(cls, name)
+        local val = rawget(_base_0, name)
+        if val == nil and _parent_0 then
+          return _parent_0[name]
+        else
+          return val
+        end
+      end,
+      __call = function(cls, ...)
+        local _self_0 = setmetatable({}, _base_0)
+        cls.__init(_self_0, ...)
+        return _self_0
+      end
+    })
+    _base_0.__class = _class_0
+    if _parent_0 and _parent_0.__inherited then
+      _parent_0.__inherited(_parent_0, _class_0)
+    end
+    Something = _class_0
   end
-  Something = _class_0
-end
-local What
-if this then
-  What = 232
-else
-  What = 4343
-end
-local d
-a, b, c, d = "hello"
-local another = 3434
-Another = 7890
-if inner then
-  local Yeah = "10000"
-end
-if this then
-  What = 232
-else
-  What = 4343
-end
-if this then
-  What = 232
-else
-  What = 4343
-end
-x, y, z = 1, 2, 3
-y = function()
-  local hallo = 3434
 end
 do
-  local _with_0 = tmp
-  local j = 2000
-  return _with_0
+  local d
+  a, b, c, d = "hello"
+end
+do
+  local What
+  if this then
+    What = 232
+  else
+    What = 4343
+  end
+  local another = 3434
+  Another = 7890
+  if inner then
+    local Yeah = "10000"
+  end
+  if this then
+    What = 232
+  else
+    What = 4343
+  end
+end
+do
+  if this then
+    local What = 232
+  else
+    local What = 4343
+  end
+  x, y, z = 1, 2, 3
+  y = function()
+    local hallo = 3434
+  end
+  do
+    local _with_0 = tmp
+    local j = 2000
+    return _with_0
+  end
 end
